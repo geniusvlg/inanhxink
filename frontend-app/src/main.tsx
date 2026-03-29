feb835269415e613d2f8f7df5a152a82cab83443
Nguyen Hoang Phuc Huy <hoangphuchuy.nguyen@zuhlke.com>
2026-03-29 13:10:05 +0700

    Add sentry

--- frontend-app/src/main.tsx
+++ frontend-app/src/main.tsx
@@ -1,8 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App'
+
+Sentry.init({
+  dsn: 'https://d79dd093cfddb2b41c149fe004c50ef1@o4511122905628672.ingest.us.sentry.io/4511126165454848',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  environment: import.meta.env.MODE,
+});
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
